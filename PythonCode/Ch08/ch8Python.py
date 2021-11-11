57a5d9f2a699afbd9e1205afc9e8e5a247a6be11
Dr James V Stone <j.v.stone@sheffield.ac.uk>
2021-11-11 17:23:07 +0000

    Add files via upload

---
+++ PythonCode/Ch08/ch8Python.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ch8Python.py. Weighted linear regression.
+This is demonstration code, so it is transparent but inefficient. 
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+from scipy import stats
+from sympy import *
+
+x = [1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,3.75,4.00]
+y = [3.34,4.97,4.15,5.40,5.21,4.56,3.69,5.86,4.58,6.94,5.57,5.62,6.87]
+sds = [0.09,0.15,0.24,0.36,0.50,0.67,0.87,1.11,1.38,1.68,2.03,2.41,2.83]
+# sds = standard deviations of each value of y.
+
+# convert sequences to vectors.   
+y0 = y
+sds=np.array(sds)
+x = np.array(x)
+y = np.array(y)
+n = len(y)
+
+###############################
+# Weighted least squares model by hand.
+###############################
+# convert vector w into matrix W etc
+w = 1 / (sds**2)
+W = np.diag(w)
+ones = np.ones(len(y))
+Xtr = [ones, x]
+Xtr = Matrix(Xtr)
+X = Xtr.T
+y = Matrix(y)
+W = Matrix(W)
+ymean = np.mean(y)
+
+# find weighted least squares solution by hand
+temp = Xtr * W * X
+tempinv = temp.inv()
+params = tempinv * Xtr * W * y
+
+b0 = params[0] # 2.122
+b1 = params[1] # 1.511
+
+print('slope b1 = %6.3f' % (b1)) 
+print('intercept b0 = %6.3f' % (b0))
+
+# convert to arrays for input to library funcitons
+y = np.array(y0)
+X = np.array(X)
+w = np.array(w)
+Xtr = [ones, x]
+X = np.transpose(Xtr)
+
+##############################################
+# WLS compare to standard WLS library output.
+##############################################
+mod_wls = sm.WLS(y, X, weights=w )
+res_wls = mod_wls.fit()
+print('\n\nWLS LIBRARY MODEL SUMMARY') 
+print(res_wls.summary())
+
+##############################################
+# OLS Estimate OLS model for comparison:
+##############################################
+res_ols = sm.OLS(y, X).fit()
+print('\n\nOLS LIBRARY MODEL SUMMARY') 
+print(res_ols.params)
+print(res_wls.params)
+print(res_ols.summary())
+
+##############################################
+# PLOT OLS and Weighted LS best fitting lines.
+##############################################
+fig = plt.figure(1)
+fig.clear()
+plt.plot(x, y, "o", label="Data")
+
+# OLS
+plt.plot(x, res_ols.fittedvalues, "r--",label="OLS")
+
+# WLS
+plt.plot(x, res_wls.fittedvalues, "g--.",label="WLS")
+plt.legend(loc="best")
+
+plt.xlabel('salary')
+plt.ylabel('height')
+
+# END OF FILE.
