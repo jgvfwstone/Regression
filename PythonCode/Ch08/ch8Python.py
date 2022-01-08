202e4043e14756cd2d51e93d9faa7f0f2119d4ea
Dr James V Stone <j.v.stone@sheffield.ac.uk>
2022-01-08 12:39:15 +0000

    Add files via upload

--- PythonCode/Ch08/ch8Python.py
+++ PythonCode/Ch08/ch8Python.py
@@ -1,53 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-ch8Python.py. Weighted linear regression.
-This is demonstration code, so it is transparent but inefficient. 
-"""
+# ch8Python.py. Weighted linear regression.
 import matplotlib.pyplot as plt
 import numpy as np
 import statsmodels.api as sm
-from scipy import stats
-from sympy import *
+import sympy as sy
 
 x = [1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,3.75,4.00]
 y = [3.34,4.97,4.15,5.40,5.21,4.56,3.69,5.86,4.58,6.94,5.57,5.62,6.87]
 sds = [0.09,0.15,0.24,0.36,0.50,0.67,0.87,1.11,1.38,1.68,2.03,2.41,2.83]
 # sds = standard deviations of each value of y.
 
-# convert sequences to vectors.   
+# Convert data to vectors.   
 y0 = y
-sds=np.array(sds)
+sds = np.array(sds)
 x = np.array(x)
 y = np.array(y)
-n = len(y)
 
 ###############################
-# Weighted least squares model by hand.
+# Weighted least squares model (WLS) using vector-matrix notation.
 ###############################
-# convert vector w into matrix W etc
+# Convert vector w into diagonal matrix W.
 w = 1 / (sds**2)
 W = np.diag(w)
 ones = np.ones(len(y))
-Xtr = [ones, x]
-Xtr = Matrix(Xtr)
-X = Xtr.T
-y = Matrix(y)
-W = Matrix(W)
+Xtr = [x, ones] # 2 rows by 13 cols.
+Xtr = sy.Matrix(Xtr)
+X = Xtr.T # transpose = 13 rows by 2 cols.
+y = sy.Matrix(y)
+W = sy.Matrix(W)
 ymean = np.mean(y)
 
-# find weighted least squares solution by hand
+# Find weighted least squares solution by hand (ie vector-matrix).
 temp = Xtr * W * X
-tempinv = temp.inv()
+tempinv = temp.inv() # invert matrix.
 params = tempinv * Xtr * W * y
 
-b0 = params[0] # 2.122
-b1 = params[1] # 1.511
+b1 = params[0] # 1.511
+b0 = params[1] # 2.122
+print('slope b1 = %6.3f' % b1) 
+print('intercept b0 = %6.3f' % b0)
 
-print('slope b1 = %6.3f' % (b1)) 
-print('intercept b0 = %6.3f' % (b0))
 
-# convert to arrays for input to library funcitons
+
+
+# Convert to arrays for input to library funcitons
 y = np.array(y0)
 X = np.array(X)
 w = np.array(w)
@@ -55,37 +52,40 @@ Xtr = [ones, x]
 X = np.transpose(Xtr)
 
 ##############################################
-# WLS compare to standard WLS library output.
+# Compare to standard WLS library output.
 ##############################################
 mod_wls = sm.WLS(y, X, weights=w )
 res_wls = mod_wls.fit()
-print('\n\nWLS LIBRARY MODEL SUMMARY') 
+print('\n\nWeighted Least Squares LIBRARY MODEL SUMMARY') 
 print(res_wls.summary())
 
 ##############################################
-# OLS Estimate OLS model for comparison:
+# Estimate OLS model for comparison:
 ##############################################
 res_ols = sm.OLS(y, X).fit()
-print('\n\nOLS LIBRARY MODEL SUMMARY') 
+print('\n\nOrdinary Least Squares LIBRARY MODEL SUMMARY') 
 print(res_ols.params)
 print(res_wls.params)
 print(res_ols.summary())
 
 ##############################################
-# PLOT OLS and Weighted LS best fitting lines.
+# PLOT Ordinary LS and Weighted LS best fitting lines.
 ##############################################
 fig = plt.figure(1)
 fig.clear()
 plt.plot(x, y, "o", label="Data")
 
-# OLS
-plt.plot(x, res_ols.fittedvalues, "r--",label="OLS")
+# Ordinary Least Squares.
+plt.plot(x,res_ols.fittedvalues,"r--",label="Ordinary Least Squares")
 
-# WLS
-plt.plot(x, res_wls.fittedvalues, "g--.",label="WLS")
+# Weighted Least Squares.
+plt.plot(x,res_wls.fittedvalues,"k-",label="Weighted Least Squares")
 plt.legend(loc="best")
 
 plt.xlabel('salary')
 plt.ylabel('height')
+plt.show()
 
+##############################################
 # END OF FILE.
+##############################################
