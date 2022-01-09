41faf0e2a36215994714becfb928a0b8e7fc944f
Dr James V Stone <j.v.stone@sheffield.ac.uk>
2022-01-09 13:53:09 +0000

    Add files via upload

--- MatLabCode/Ch03/Ch3MatLab.m
+++ MatLabCode/Ch03/Ch3MatLab.m
@@ -1,17 +1,35 @@
-% File: Ch3MatLab.m.
+% File: Ch3MatLab.m
+% Find coefficient of determination = r2.
 
 % load data
 x=[1.00 1.25 1.50 1.75 2.00 2.25 2.50 2.75 3.00 3.25 3.50 3.75 4.00]';
 y=[3.34 4.97 4.15 5.40 5.21 4.56 3.69 5.86 4.58 6.94 5.57 5.62 6.87]';
 
-c = cov(x,y,1);  
-c = c(1,2);
+n = length(y);
+numparams = 2; % slope and intercept.
 
-varx = var(x,1); % 0.875
-vary = var(y,1); % 1.095
+% Estimate slope b1.
+xmean = mean(x);    
+ymean = mean(y);
 
-r2 = c^2 / (varx * vary); % 0.4665
+numerator = sum( (x-xmean) .* (y-ymean) ) / n;
+denominator = sum( (x-xmean).^2 ) / n;
 
-fprintf('r2 = %.3f\n',r2);  
+b1 = numerator/denominator; % slope
+
+% Estimate intercept b0.
+b0 = ymean - b1*xmean;
+
+% Get projection of data onto best fitting line.
+yhat = b1 .* x + b0; % points on line.
+
+% Find coefficient of determination = r2.
+varyhat = var(yhat,1); % Variance of yhat.
+vary = var(y,1); % Variance of y.
+r2 = varyhat / vary;
+
+fprintf('Variance of yhat = %.3f\n',varyhat);  
+fprintf('Variance of y = %.3f\n',vary);  
+fprintf('Coefficient of determination, r2 = varyhat/vary = %.3f\n',r2);  
 
 % END OF FILE.
