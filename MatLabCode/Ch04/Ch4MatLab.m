cda59393538f4a125799cec2cfae3725e91f4eaa
Dr James V Stone <j.v.stone@sheffield.ac.uk>
2022-01-09 13:53:31 +0000

    Add files via upload

--- MatLabCode/Ch04/Ch4MatLab.m
+++ MatLabCode/Ch04/Ch4MatLab.m
@@ -1,29 +1,30 @@
-% File: Ch4MatLab.m.
+% File: Ch4MatLab.m. The mTCDF function is listed in Appendix F.
+% Statistical Significance of the Mean.
 
-x=[1.00 1.25 1.50 1.75 2.00 2.25 2.50 2.75 3.00 3.25 3.50 3.75 4.00]';
 y=[3.34 4.97 4.15 5.40 5.21 4.56 3.69 5.86 4.58 6.94 5.57 5.62 6.87]';
 
 n = length(y);
 ymean = mean(y);
-fprintf('ymean = %.3f\n',ymean); % = 5.135
+fprintf('ymean = %.3f.\n',ymean); % = 5.135
 
-% variance of y.
-vary = 1/n * sum( (y-ymean).^2 );
-fprintf('vary = %.3f\n',vary); % = 1.095
+% Variance of sample.
+vary = 1/n * sum( (y-ymean).^2 ); % = 1.095
+fprintf('Variance of sample = %.3f.\n',vary); 
 
-% estimated parent population variance.
-varhaty =  1/(n-1) * sum( (y-ymean).^2 );
-fprintf('varhaty = %.3f\n',varhaty); % = 1.187
+% Estimated (un-biased) variance of parent population.
+varhaty =  1/(n-1) * sum( (y-ymean).^2 ); % = 1.187
+fprintf('Estimated variance of parent population = %.3f.\n',varhaty); 
 
-% standard error of ymean.
+% Find standard error of ymean.
 semymean = sqrt( varhaty / n);
-fprintf('semymean = %.3f\n',semymean); % =  0.302
+fprintf('Standard error of mean = %.3f.\n',semymean); % =  0.302
 
-% t-value for tymean
+% Find t-value for ymean.
 tymean = ymean/semymean; % 16.99
-fprintf('tymean = %.3f\n',tymean); % = 16.997
+fprintf('t-value for the mean = %.3f.\n',tymean); % = 16.997
 
-p=mTCDF(tymean,n-1);
-fprintf('p = %.3e\n',p); % p = 9.234e-10
+% Find p-value for ymean.
+p = mTCDF(tymean,n-1); 
+fprintf('p-value for the mean = %.3e.\n',p); % p = 9.234e-10
 
 % END OF FILE.
