6c6ffeb2d3d122d15300b2e0ca7703b9150764d2
Dr James V Stone <j.v.stone@sheffield.ac.uk>
2022-01-09 13:52:45 +0000

    Add files via upload

--- MatLabCode/Ch02/Ch2MatLab.m
+++ MatLabCode/Ch02/Ch2MatLab.m
@@ -1,4 +1,5 @@
-% File: Ch2MatLab.m.
+% File: Ch2MatLab.m
+% Estimate slope and intercept.
 
 % load data
 x=[1.00 1.25 1.50 1.75 2.00 2.25 2.50 2.75 3.00 3.25 3.50 3.75 4.00]';
@@ -6,29 +7,27 @@ y=[3.34 4.97 4.15 5.40 5.21 4.56 3.69 5.86 4.58 6.94 5.57 5.62 6.87]';
 
 n = length(y); % number of data points.
 
-% Estimate slope.
+% Estimate slope = b1.
 xmean = mean(x);
 ymean = mean(y);
 numerator = sum( (x-xmean) .* (y-ymean) ) / n;
 denominator = sum( (x-xmean).^2 ) / n;
+% Of course, the term n cancels in this ratio. 
 b1 = numerator/denominator; % slope
 
-% Estimate intercept.
+% Estimate intercept = b0.
 b0 = ymean - b1*xmean;
 
 fprintf('Slope = %.3f\n',b1);        % 0.764
 fprintf('Intercept = %.3f\n',b0);   % 3.225
 
-yhat = b1*x + b0;
-r2 = var(yhat,1) / var(y,1); % 0.4665
-
 % Plot best fitting line through data.
 xforline = 0:0.1:6;
-yhatforline = b1.*xforline + b0; % points on line.
+yhatforline = b1 .* xforline + b0; % points on line.
 
 % Plot fitted line xest (=xhat in text) and data points.
 figure(1); clf; hold on;
-plot(xforline,yhatforline,'k--'); % plot best fitting line.
+plot(xforline,yhatforline,'k-'); % plot best fitting line.
 plot(x,y, 'k.', 'MarkerSize',20); % plot data.
 set(gca,'Linewidth',2); 
 set(gca,'FontSize',20);
